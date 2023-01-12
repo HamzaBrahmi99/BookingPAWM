893f3bdee5e597a4a9323a83ca8172ec816d6567
Hamza Brahmi <hamza.brahmi@outlook.it>
2023-01-12 12:22:54 +0100

    Inizio pagina descrizione degli hotel

--- src/components/searchItem/SearchItem.jsx
+++ src/components/searchItem/SearchItem.jsx
@@ -4,8 +4,15 @@ export const SearchItem = () => {
   return (
     <div className="searchItem">
         <img src="https://cf.bstatic.com/xdata/images/hotel/square600/72905401.webp?k=abcb71543a55a5d7cb7ce27869da1816a77efdf341c4eab2405ad2afbf5e7187&o=&s=1" alt="" className="siImg" />
-        <div className="siDesc">adsfasdfadsafsdfsa</div>
-        <div className="siDetails">asfasfdsfadsfdsafdsf</div>
+        <div className="siDesc">
+            <h1 className="siTitle">Da Foschetta</h1>
+            <span className="siSubtitle">Camera Matrimoniale/Doppia con Letti Singoli con Vista Giardino</span>
+            <span className="siFeatures">Letti: 1 matrimoniale o 2 singoli</span>
+        </div>
+        <div className="siDetails">
+            <div className="siPrice">€400</div>
+            <button className="siCheckButton">Vedi disponibilità</button>
+        </div>
     </div>
   )
 }
--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import "./hotel.css"
+import Navbar from "../../components/navbar/Navbar"
+import Header from "../../components/header/Header"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons"
+
 
 function Hotel() {
   return (
-    <div>Hotel</div>
+    <div>
+      <Navbar/>
+      <Header type="list"/>
+      <div className="hotelContainer">
+        <div className="hotelWrapper">
+          <h1 className="hotelTitle">Grand Hotel</h1>
+          <div className="hotelAddress">
+            <FontAwesomeIcon incon={faLocationDot}/>
+            <span>Elton St 125 New York</span>
+          </div>
+          <div className="hotelImages">
+            <div className="hotelImgWrapper">
+              <img src="https://cf.bstatic.com/xdata/images/hotel/max1280x900/121399207.jpg?k=16e7a6131ee3cc1bc92346d473b167d92eb9be1a35172e03db8426b47041e91f&o=&hp=1" alt="" className="hotelImg" />
+            </div>
+          </div>
+          <div className="hotelDetails">
+            <div className="hotelDetailsText">
+              <h1 className="hotelTitle">Soggiorna nel cuore di Praga</h1>
+              <p className="hotelDesc">Ex residenza di Re Giorgio, il 3 Epoques Apartments by Prague Residences è il
+               più antico edificio rimasto sulla riva destra del Fiume Moldava a Praga e dista appena 4 minuti
+                a piedi dalla Piazza della Città Vecchia e dal Ponte Carlo.
+              </p>
+            </div>
+            <div className="hotelDetailsPrice">
+              <h1>Completa la prenotazione!</h1>
+              <b>€400</b>
+              <button>Prenota ora</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
